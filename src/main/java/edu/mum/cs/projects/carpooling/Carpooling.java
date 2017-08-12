71c97faa538cf46b856696fe8d4b2bf132e12fe0
Destalem <destalemtes@gmail.com>
2017-08-12 10:30:46 -0500

    readme text added

--- src/main/java/edu/mum/cs/projects/carpooling/Carpooling.java
+++ src/main/java/edu/mum/cs/projects/carpooling/Carpooling.java
@@ -13,10 +13,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
-public class carpooling {
+public class Carpooling {
 
 	public static void main(String[] args) {
-		SpringApplication.run(carpooling.class, args);
+		SpringApplication.run(Carpooling.class, args);
 	}
 
 }
